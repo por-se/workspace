4350476b53a1863ba56e6981ac1c488a751149cf
Felix Rath <felix.rath@comsys.rwth-aachen.de>
2019-01-07 18:58:47 +0100

    also allow full parallel link when total mem is >=35G
    closes #1

--- workspace_base/workspace_base/recipes/llvm.py
+++ workspace_base/workspace_base/recipes/llvm.py
@@ -69,9 +69,9 @@ class LLVM(Recipe):
             if self.profile == "debug":
                 cmake_args += ["-DCMAKE_BUILD_TYPE=Debug"]
                 avail_mem = psutil.virtual_memory().available
-                if avail_mem < ws.args.num_threads * 12000000000:
+                if avail_mem < ws.args.num_threads * 12000000000 and avail_mem < 35000000000:
                     print(
-                        "[LLVM] less than 12G memory per thread available during a debug build; restricting link-parallelism to 1 [-DLLVM_PARALLEL_LINK_JOBS=1]"
+                        "[LLVM] less than 12G memory per thread (or 35G total) available during a debug build; restricting link-parallelism to 1 [-DLLVM_PARALLEL_LINK_JOBS=1]"
                     )
                     cmake_args += ["-DLLVM_PARALLEL_LINK_JOBS=1"]
             elif self.profile == "release":
