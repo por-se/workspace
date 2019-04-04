d6db007c8d64c383113652ce194238a8b4634dab
Frederic Kehrein <frederic@kehrein.org>
2019-04-04 15:40:20 +0200

    Added STP binary to PATH

--- ws-src/workspace/recipes/stp.py
+++ ws-src/workspace/recipes/stp.py
@@ -137,3 +137,6 @@ class STP(Recipe):
             if self.paths.src_dir.is_dir():
                 shutil.rmtree(self.paths.src_dir)
             ws.git_remove_exclude_path(self.paths.src_dir)
+
+    def add_to_env(self, env, ws: Workspace):
+        env_prepend_path(env, "PATH", self.paths.build_dir)
