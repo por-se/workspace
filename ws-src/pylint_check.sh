1c1477add7e6155955d5c58fb6a190c6d0342945
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2019-08-01 12:24:07 +0200

    add pylint

---
+++ ws-src/pylint_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -u
+set -e
+set -o pipefail
+
+# adapted from https://stackoverflow.com/a/246128/65678
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$(dirname "$SOURCE")" && pwd )"
+cd "$DIR"/..
+
+export PIPENV_VENV_IN_PROJECT=1
+if [[ ! -d .venv ]] || [[ Pipfile -nt Pipfile.lock ]] || [[ ! -x .venv/bin/pylint ]] ; then
+	if [[ -r /etc/issue ]] && [[ "$(cat /etc/issue)" = 'Debian'* ]] ; then
+		# https://github.com/pypa/pipenv/issues/1744
+		>&2 echo "Performing workaround for Debian"
+		pushd ws-src
+		pipenv run python setup.py develop
+		popd
+	fi
+	pipenv update -d
+fi
+exec pipenv run sh -c "cd ws-src && exec pylint workspace"
