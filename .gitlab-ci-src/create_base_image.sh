4771da54c99926b642a2f8b4487bc502ae91cbd3
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2019-08-05 13:20:01 +0200

    Build a better docker container

--- .gitlab-ci-src/create_base_image.sh
+++ .gitlab-ci-src/create_base_image.sh
@@ -9,9 +9,14 @@ set -v # print commands to CI output
 
 docker build --cache-from=$IMAGE_NAME:ci -f .gitlab-ci-src/base.Dockerfile -t $IMAGE_NAME:ci .
 docker run --name sources -v ~/netrc:/root/.netrc -v /cache:/cache $IMAGE_NAME:ci bash -c "set -e ; set -u ; set -o pipefail
+	cd /workspace
 	cp .gitlab-ci-src/ws-settings.toml .
 	PIPENV_CACHE_DIR=/cache/pipenv ./ws setup
 "
-docker commit --change "CMD bash" sources base
+docker commit \
+	--change 'WORKDIR /workspace' \
+	--change 'ENTRYPOINT ["/workspace/ws"]' \
+	--change 'CMD ["shell", "-s", "fish"]' \
+	sources base
 
 # at this point cleanup might be performed, but we just wait for the cleanup of the dind container
