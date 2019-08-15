37abab3f6332b1e54e7cb2f35061346ce302ae5a
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2019-08-15 15:37:17 +0200

    Add isort

---
+++ ws-src/isort_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -u
+set -e
+set -o pipefail
+
+# adapted from https://stackoverflow.com/a/246128/65678
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$(dirname "$SOURCE")" && pwd )"
+cd "$DIR"
+
+exec ../ws isort --check --diff -w 120 --recursive ws-src/workspace ws-src/setup.py
