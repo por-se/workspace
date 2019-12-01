2f5ac40f03066df5ce1a454c7cb70046ecc35620
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2019-12-01 22:13:41 +0100

    z3 4.8.7

--- ws-src/workspace/recipes/z3.py
+++ ws-src/workspace/recipes/z3.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict
 
 from workspace.build_systems.cmake_recipe_mixin import CMakeRecipeMixin
@@ -18,6 +19,12 @@ class Z3(Recipe, GitRecipeMixin, CMakeRecipeMixin):  # pylint: disable=invalid-n
     """The [z3](https://github.com/Z3Prover/z3) constraint solver"""
 
     profiles = {
+        "lto": {
+            "cmake_args": {
+                'CMAKE_BUILD_TYPE': 'Release',
+                'Z3_LINK_TIME_OPTIMIZATION': True,
+            },
+        },
         "release": {
             "cmake_args": {
                 'CMAKE_BUILD_TYPE': 'Release',
@@ -44,22 +51,28 @@ class Z3(Recipe, GitRecipeMixin, CMakeRecipeMixin):  # pylint: disable=invalid-n
     }
 
     default_arguments: Dict[str, Any] = {
+        "gmp": True,
         "shared": True,
-        "openmp": True,
+        "thread-safe": True,
     }
 
     argument_schema: Dict[str, Any] = {
+        "gmp": bool,
         "shared": bool,
-        "openmp": bool,
+        "thread-safe": bool,
     }
+
+    @property
+    def gmp(self) -> bool:
+        return self.arguments["gmp"]
 
     @property
     def shared(self) -> bool:
         return self.arguments["shared"]
 
     @property
-    def openmp(self) -> bool:
-        return self.arguments["openmp"]
+    def thread_safe(self) -> bool:
+        return self.arguments["thread-safe"]
 
     def __init__(self, **kwargs):
         GitRecipeMixin.__init__(self, "github://Z3Prover/z3.git")
@@ -81,14 +94,27 @@ class Z3(Recipe, GitRecipeMixin, CMakeRecipeMixin):  # pylint: disable=invalid-n
         Recipe.compute_digest(self, workspace, digest)
         CMakeRecipeMixin.compute_digest(self, workspace, digest)
 
+        digest.update(f'gmp:{self.gmp}'.encode())
         digest.update(f'shared:{self.shared}'.encode())
-        digest.update(f'openmp:{self.openmp}'.encode())
+        digest.update(f'thread-safe:{self.thread_safe}'.encode())
 
     def configure(self, workspace: Workspace):
         CMakeRecipeMixin.configure(self, workspace)
 
-        self.cmake.set_flag("BUILD_LIBZ3_SHARED", self.shared)
-        self.cmake.set_flag("USE_OPENMP", self.openmp)
+        self.cmake.set_flag("Z3_BUILD_LIBZ3_SHARED", self.shared)
+        self.cmake.set_flag("Z3_USE_LIB_GMP", self.gmp)
+        # renamed on master to Z3_SINGLE_THREADED
+        self.cmake.set_flag("SINGLE_THREADED", not self.thread_safe)
+
+    def build(self, workspace: Workspace):
+        CMakeRecipeMixin.build(self, workspace)
+
+        with open(self.paths["build_dir"] / "CMakeCache.txt") as f:
+            for line in f:
+                if line.startswith("GMP_CXX_LIBRARIES:FILEPATH="):
+                    self.paths["libgmpxx"] = Path(line[len("GMP_CXX_LIBRARIES:FILEPATH="):].strip())
+                if line.startswith("GMP_C_LIBRARIES:FILEPATH="):
+                    self.paths["libgmp"] = Path(line[len("GMP_C_LIBRARIES:FILEPATH="):].strip())
 
     def add_to_env(self, env, workspace: Workspace):
         env_prepend_path(env, "PATH", self.paths["build_dir"])
