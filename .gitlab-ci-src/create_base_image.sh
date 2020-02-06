7fd614c407f74888d6a660280157363065b96c0e
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2020-02-06 21:26:35 +0100

    Work further on registry change

--- .gitlab-ci-src/create_base_image.sh
+++ .gitlab-ci-src/create_base_image.sh
@@ -7,7 +7,13 @@ set -o pipefail
 
 set -v # print commands to CI output
 
-docker build --pull --cache-from=$CI_REGISTRY_IMAGE/ci:latest -f .gitlab-ci-src/base.Dockerfile -t $CI_REGISTRY_IMAGE/ci:latest .
+# pull docker base image with token, such that the CI user does not need access to it
+docker login -u $DOCKER_BASE_IMAGE_USER -p $DOCKER_BASE_IMAGE_PASSWORD laboratory.comsys.rwth-aachen.de:5050
+docker pull laboratory.comsys.rwth-aachen.de:5050/symbiosys/projects/workspace/docker-base-image:latest
+# log back in with the ci token
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+
+docker build --cache-from=$CI_REGISTRY_IMAGE/ci:latest -f .gitlab-ci-src/base.Dockerfile -t $CI_REGISTRY_IMAGE/ci:latest .
 docker run --name sources -v ~/.netrc:/root/.netrc -v /cache:/cache $CI_REGISTRY_IMAGE/ci:latest bash -c "set -e ; set -u ; set -o pipefail
 	cd /workspace
 	cp .gitlab-ci-src/ws-settings.toml .
@@ -17,6 +23,6 @@ docker commit \
 	--change 'WORKDIR /workspace' \
 	--change 'ENTRYPOINT ["/workspace/ws"]' \
 	--change 'CMD ["shell", "-s", "fish"]' \
-	sources base
+	sources $CI_REGISTRY_IMAGE/ci:$CI_COMMIT_SHA
 
 # at this point cleanup might be performed, but we just wait for the cleanup of the dind container
